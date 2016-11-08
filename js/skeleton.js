1901d918990eb646a906697feb1c37cf72627043
BrandonCrowther <bcrowthe@unb.ca>
2016-11-08 15:33:20 -0400

    More setup. Skeleton javascript+css files, link index.html to its own js file

---
+++ js/skeleton.js
@@ -0,0 +1,10 @@
+//Skeleton js file
+
+
+// anything in this block will execute as soon as the page finishes loading
+// all button bindings and most jquery should be bound in here
+$(function(){  
+
+
+
+});
