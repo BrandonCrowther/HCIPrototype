284c969b1026b1a6f16afaa6af689e8b8b45096f
BrandonCrowther <bcrowthe@unb.ca>
2016-11-13 15:53:13 -0400

    Added schools, sample skeleton for school table

---
+++ js/schools.js
@@ -0,0 +1,22 @@
+//Skeleton js file
+
+
+// anything in this block will execute as soon as the page finishes loading
+// all button bindings and most jquery should be bound in here
+$(function(){
+  provinceChanged();
+});
+
+
+function provinceChanged(){
+  var province = $("#province_selector").find("option:selected").val();
+  var list_elements = $("#school_selector").find('option');
+  list_elements.each(function(e){
+    var elem = $(this);
+    if(elem.data('prov') == province){
+      elem.show();
+      $("#school_selector").val(elem);
+    }
+    else elem.hide();
+  });
+}
