8a91b346524d6475c48faa3d9e42bb4297db2906
BrandonCrowther <bcrowthe@unb.ca>
2016-11-13 20:44:16 -0400

    Added tagging for videos

--- js/videos.js
+++ js/videos.js
@@ -1,11 +1,15 @@
 $(function(){
   //Copy iframe sample and embed each youtube iframe into the skeleton
-  var video_urls = ["https://www.youtube.com/embed/CLishDbwy9c", "https://www.youtube.com/embed/NUC2EQvdzmY"]
+  var videos =
+  {
+    "https://www.youtube.com/embed/CLishDbwy9c": "Rap TestTag", //these are tags to use for filtering. Add your own easily
+    "https://www.youtube.com/embed/NUC2EQvdzmY": "StillRap"
+  };
   var iframeSample = $('#sample_iframe iframe');
   var container = $('#video_panel')
-  video_urls.forEach(function(ele){
+  Object.keys(videos).forEach(function(ele){
     iframeSample.attr('src', ele);
-    console.log(ele);
+    iframeSample.attr('data-tags', videos[ele]);
     container.append(iframeSample.clone());
   });
 });
